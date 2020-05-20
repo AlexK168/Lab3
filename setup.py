bf6ac2cc4e9b07e2fef3d6eded4bc62fa331203e
Alex168 <sashok1356@gmail.com>
2020-05-20 11:33:14 +0300

    setup added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Amazon',
+    version='1.1',
+    packages=['shop', 'shop.migrations', 'Amazon'],
+    url='',
+    license='',
+    author='alex',
+    author_email='',
+    description=''
+)
